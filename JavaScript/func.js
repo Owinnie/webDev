4f5e04defae2442d1b0c23d56bdc3f5b6489fa81
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-10 16:23:48 +0300

    functions

--- JavaScript/func.js
+++ JavaScript/func.js
@@ -8,3 +8,35 @@ function costOfMonitors(rows, columns) {
 
 const totalCost = costOfMonitors(5, 4);
 console.log(totalCost);
+
+// Anonymous functions
+const waterThePlant = function (day){
+  if(day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(waterThePlant('Tuesday'));    // Output: false
+
+// Arrow functions
+const waterThePlant = (day)=> {
+  if (day === 'Wednesday') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(waterThePlant('Tuesday'));    // Output: false
+
+// Single line arrow function
+const waterThePlant = day => day === 'Wednesday' ? true : false;    // tenary if --> iif
+
+console.log(waterThePlant('Tuesday'));    // Output: false
+
+// display "Hello" in the inner HTML of an element with the ID "demo"
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello";
+}
