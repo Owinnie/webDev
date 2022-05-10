aa37dba5b184085ab793c832c3bcdc4d2f9a4186
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-10 15:30:38 +0300

    typeof operator

---
+++ JavaScript/typeof.js
@@ -0,0 +1,9 @@
+// returns type of a value
+const value = '10';
+console.log(typeof value); // Output: string
+ 
+const value2 = 10;
+console.log(typeof value2); // Output: number
+ 
+const value3 = false; 
+console.log(typeof value3); // Output: boolean
