a1789da3cafdd6ec68f90773ee5256723e58c300
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-10 14:56:15 +0300

    String Interpolation

---
+++ JavaScript/strInterpolation.js
@@ -0,0 +1,5 @@
+// use of backticks ``
+// use placeholder --> ${}
+var chString = 'This is';
+var cityName = 'Kanairo (slang for Nairobi)';
+console.log(`Welcome. ${chString} ${cityName}`);    // Welcome. This is Kanairo (slang for Nairobi)
