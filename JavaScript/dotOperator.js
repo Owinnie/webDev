27a1b5c0fcba229d3a3b3650995e8fbcff244863
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-10 10:45:59 +0300

    The dot '.' operator
    String Methods

---
+++ JavaScript/dotOperator.js
@@ -0,0 +1,24 @@
+console.log('Despite its technicalities, I love coding'.length);
+
+// Output
+41
+
+/*
+String methods()
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+*/
+
+console.log('owinnie'.toUpperCase());
+// OWINNIE
+
+console.log('    Whitespace   '.trim());
+// Whitespace
+
+console.log('Hi'.startsWith('H'));
+// true
+
+console.log('Hi'.startsWith('h'));    // case-sensitive
+// false
+
+console.log('    Whitespace   '.trim().toUpperCase());
+// WHITESPACE
