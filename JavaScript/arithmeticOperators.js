adc30e1ae829ee0a1cda8aec246c70b3f6db8105
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-10 10:23:55 +0300

    Arithmetic Operators

---
+++ JavaScript/arithmeticOperators.js
@@ -0,0 +1,12 @@
+// + --> addition; string concatenation
+// - --> subtraction
+// * --> multiplication
+// / --> division
+// % --> remainder : modulo
+
+console.log(3 + 4);
+console.log('One' + ', ' + 'two' + ', ' + 'three');   // One, two, three
+console.log(4 - 3);
+console.log(3 * 4);
+console.log(4 / 2);
+console.log(9 % 3);
