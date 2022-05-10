c2a49e40710bf11b2bb3dd79d4ec89c674366ed5
Owinnie <99328720+Owinnie@users.noreply.github.com>
2022-05-10 10:08:16 +0300

    Data Types

---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,17 @@
+/* Data type in JavaScript
+Strings --> '' or ""
+Numbers --> int, float
+Boolean --> true, false
+Null : exists but absent --> null
+Undefined : doesn't exist --> undefined
+Objects
+*/
+
+
+// Strings
+console.log('My name is Winnie');
+console.log('I love coding in Javascript');
+
+// Numbers
+console.log(2022);
+console.log(20);
